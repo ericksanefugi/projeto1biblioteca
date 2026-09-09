59ea29a94eec8fe9a2b4963e29d2e27235f6bb7d
Erick Leiss <erick.sanefugi@escola.pr.gov.br>
2026-09-09 11:45:34 -0300

    colocar o script do sql e arrumamo os aluno

--- app.py
+++ app.py
@@ -76,3 +76,4 @@ def listar_alunos():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 DB_CONFIG = {
     "host": "localhost",
     "user": "biblioteca_user1",
-    "password": "SUA_SENHA",
+    "password": "projeto1",
     "database": "biblioteca_1ano"
 }
