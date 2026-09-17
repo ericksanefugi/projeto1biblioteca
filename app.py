0b8fb566ae5760c74cb9cf3c6dde3c31f5b5f208
Erick Leiss <erick.sanefugi@escola.pr.gov.br>
2026-09-17 11:55:31 -0300

    adicionar o cadastro de bibliotecario

--- app.py
+++ app.py
@@ -142,5 +142,72 @@ def cadastrar_livro():
         return f"Erro ao cadastrar livro: {erro}"
 
 
+@app.route("/bibliotecarios")
+def listar_bibliotecarios():
+    try:
+        conexao = conectar()
+        cursor = conexao.cursor(dictionary=True)
+
+
+        cursor.execute("SELECT * FROM bibliotecarios")
+        bibliotecarios = cursor.fetchall()
+
+
+        cursor.close()
+        conexao.close()
+
+
+        return render_template("bibliotecario.html", bibliotecarios=bibliotecarios)
+
+
+    except Exception as erro:
+        return f"Erro ao listar bibliotecários: {erro}"
+
+
+
+
+@app.route("/bibliotecarios/novo")
+def formulario_bibliotecario():
+    return render_template("bibliotecario_form.html")
+
+
+
+
+@app.route("/bibliotecarios/cadastrar", methods=["POST"])
+def cadastrar_bibliotecario():
+    try:
+        nome = request.form["nome"]
+        email = request.form["email"]
+
+
+        conexao = conectar()
+        cursor = conexao.cursor()
+
+
+        sql = """
+            INSERT INTO bibliotecarios (nome, email)
+            VALUES (%s, %s)
+        """
+
+
+        valores = (nome, email)
+
+
+        cursor.execute(sql, valores)
+        conexao.commit()
+
+
+        cursor.close()
+        conexao.close()
+
+
+        return redirect("/bibliotecarios")
+
+
+    except Exception as erro:
+        return f"Erro ao cadastrar bibliotecário: {erro}"
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
